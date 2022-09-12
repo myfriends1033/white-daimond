372f7f34b03eba4fa9ad03c8be2a5f761df8164a
prettypolice <myfriends1033@gmail.com>
2022-09-12 22:17:23 +0800

    increase fade in height

--- js/backtop.js
+++ js/backtop.js
@@ -3,10 +3,10 @@ $(function(){
       $('html,body').animate({scrollTop:0}, 333);
     });
     $(window).scroll(function() {
-      if ( $(this).scrollTop() > 100 ){
-        $('#BackTop').fadeIn(122);
+      if ( $(this).scrollTop() > 150 ){
+        $('#BackTop').fadeIn(172);
       } else {
-        $('#BackTop').stop().fadeOut(122);
+        $('#BackTop').stop().fadeOut(172);
       }
     }).scroll();
   });
