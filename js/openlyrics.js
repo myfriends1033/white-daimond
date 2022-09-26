ddd562e3187a2107044a7baa0e35758e23bb41c3
prettypolice <myfriends1033@gmail.com>
2022-09-26 22:39:48 +0800

    Add files via upload

---
+++ js/openlyrics.js
@@ -0,0 +1,20 @@
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "https://myfriends1033.github.io/white-diamond/other/lyrics", true);
+xhr.send();
+//xhr.responseText;
+document.getElementById("lyricspage").innerHTML = xhr.responseText;
+
+
+function infor1open() {
+    document.getElementById("infor1").style.cssText += 'display:block';
+}
+
+function infor2open() {
+    document.getElementById("infor2").style.cssText += 'display:block';
+}
+
+function lyclose() {
+    for ( let i = 0 ; i < document.querySelectorAll(".ly").length; i++) {
+        document.querySelectorAll(".ly")[i].style.cssText += 'display:none';
+    }
+}
