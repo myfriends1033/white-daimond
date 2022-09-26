c8d042399cc192128ba760afd900806850f5c0e3
prettypolice <myfriends1033@gmail.com>
2022-09-27 00:16:36 +0800

    Update openlyrics.js

--- js/openlyrics.js
+++ js/openlyrics.js
@@ -1,5 +1,14 @@
+// const xhr = new XMLHttpRequest();
+// xhr.open("GET", "https://myfriends1033.github.io/white-diamond/other/lyrics", true);
+// xhr.send();
+// //xhr.responseText;
+// document.getElementById("lyricspage").innerHTML = xhr.responseText;
+
+$('#lyricspage').load("https://myfriends1033.github.io/white-diamond/other/lyrics");
+
+
 function infor1open() {
-    document.getElementById("infor1").style.cssText += 'display:block';
+  document.getElementById("infor1").style.cssText += 'display:block';
 }
 
 function infor2open() {
