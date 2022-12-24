574071a95097096de3adb6e410c6963f80fc5f2f
prettypolice <myfriends1033@gmail.com>
2022-12-25 01:35:21 +0800

    Add files via upload

--- js/backtop.js
+++ js/backtop.js
@@ -1,12 +1,30 @@
-$(function(){
-    $('#BackTop').click(function(){ 
-      $('html,body').animate({scrollTop:0}, 333);
-    });
-    $(window).scroll(function() {
-      if ( $(this).scrollTop() > 150 ){
-        $('#BackTop').fadeIn(172);
-      } else {
-        $('#BackTop').stop().fadeOut(172);
-      }
-    }).scroll();
-  });
+// $(function(){
+//     $('#BackTop').click(function(){ 
+//       $('html,body').animate({scrollTop:0}, 333);
+//     });
+//     $(window).scroll(function() {
+//       if ( $(this).scrollTop() > 100 ){
+//         $('#BackTop').fadeIn(122);
+//       } else {
+//         $('#BackTop').stop().fadeOut(122);
+//       }
+//     }).scroll();
+//   });
+
+
+// here use body onscroll or need to use scroll element listener
+function topbfade() {
+  if (document.body.scrollTop <= 100) {
+    document.getElementById('BackTop').style.cssText += 'display:none';
+  } else {
+    document.getElementById('BackTop').style.cssText += 'display:block';
+  }
+}
+
+// back to top buttom add onclick
+function backtotop() {
+  document.body.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+}
